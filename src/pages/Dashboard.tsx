09969a21a1ffd73eb5736124484549d533bf7240
Aimee Nguyen <aimeeqnguyen@outlook.com>
2023-06-21 09:59:39 -0700

    Updated dependenies and pages

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 
+
 const Dashboard = () => {
 	const router = useRouter();
 
 	return (
 		<div>
 			<h1>Doggie Dashboard</h1>
-			<button onClick={() => router.push('/')}>Go back home</button>
-			
-
+			<button onClick={() => router.push('/')}>Sign Out</button>
+			<button>Find Friends</button>
+			<button onClick={() => router.push('/editTag')}>Edit DogTag</button>
 		</div>
 	);
 };
---
+++ src/pages/editTag.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const EditTag = () => {
+  return(
+    <div>
+      <h1>Edit Dog Tag</h1>
+    </div>
+  )
+};
+export default EditTag;;
